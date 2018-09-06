665f342ef1efbbcfb6fbb7a980a1503e10c6813f
sanke <sanke@DESKTOP-K20R6LF>
2018-09-07 00:23:20 +0200

    ok

--- Selenium_Basics/src/self_study/Introduction.java
+++ Selenium_Basics/src/self_study/Introduction.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Introduction {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
 		//Create Driver Object X driver=new X();
 		//Path:C:\Users\sanke\Documents\chromedriver.exe
